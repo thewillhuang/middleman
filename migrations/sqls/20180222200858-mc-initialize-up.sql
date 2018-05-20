367547e46cd25ed5bd98923486b3fd7b9d8f90aa
William Huang <will.h86@gmail.com>
2018-05-19 23:43:21 -0700

    more caps on sql cmds

--- migrations/sqls/20180222200858-mc-initialize-up.sql
+++ migrations/sqls/20180222200858-mc-initialize-up.sql
@@ -1,15 +1,14 @@
--- CREATE EXTENSION IF NOT EXISTS postgis;
-CREATE schema m_priv;
-CREATE schema m_pub;
+CREATE SCHEMA m_priv;
+CREATE SCHEMA m_pub;
 CREATE EXTENSION IF NOT EXISTS postgis;
 ALTER DEFAULT PRIVILEGES REVOKE EXECUTE ON FUNCTIONS FROM public;
 
 CREATE FUNCTION m_pub.set_updated_at() RETURNS TRIGGER AS $$
-begin
+BEGIN
   new.updated_at := current_timestamp;
-  return new;
-end;
-$$ language plpgsql;
+  RETURN new;
+END;
+$$ LANGUAGE plpgsql;
 
 CREATE TABLE m_pub.phone (
   id BIGSERIAL PRIMARY KEY,
@@ -153,34 +152,34 @@ CREATE TABLE m_priv.person_account (
   password_hash    TEXT NOT NULL
 );
 
-COMMENT ON TABLE m_priv.person_account is 'Private information about a person’s account.';
-COMMENT ON COLUMN m_priv.person_account.person_id is 'The id of the person associated with this account.';
-COMMENT ON COLUMN m_priv.person_account.email is 'The email address of the person.';
-COMMENT ON COLUMN m_priv.person_account.password_hash is 'An opaque hash of the person’s password.';
+COMMENT ON TABLE m_priv.person_account IS 'Private information about a person’s account.';
+COMMENT ON COLUMN m_priv.person_account.person_id IS 'The id of the person associated with this account.';
+COMMENT ON COLUMN m_priv.person_account.email IS 'The email address of the person.';
+COMMENT ON COLUMN m_priv.person_account.password_hash IS 'An opaque hash of the person’s password.';
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE FUNCTION m_pub.register_person(
-  first_name text,
-  last_name text,
-  email text,
-  password text
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  password TEXT
 ) RETURNS m_pub.person AS $$
 declare
   person m_pub.person;
-begin
-  INSERT INTO m_pub.person (first_name, last_name) values
+BEGIN
+  INSERT INTO m_pub.person (first_name, last_name) VALUES
     (first_name, last_name)
-    returning * INTO person;
+    RETURNING * INTO person;
 
-  INSERT INTO m_priv.person_account (person_id, email, password_hash) values
+  INSERT INTO m_priv.person_account (person_id, email, password_hash) VALUES
     (person.id, email, crypt(password, gen_salt('bf')));
 
-  return person;
-end;
-$$ language plpgsql strict security definer;
+  RETURN person;
+END;
+$$ LANGUAGE plpgsql strict security definer;
 
-COMMENT ON FUNCTION m_pub.register_person(TEXT, TEXT, TEXT, TEXT) is 'Registers a single person and creates an account in our forum.';
+COMMENT ON FUNCTION m_pub.register_person(TEXT, TEXT, TEXT, TEXT) IS 'Registers a single person and creates an account in our forum.';
 
 CREATE ROLE sys_admin LOGIN PASSWORD 'voodoo3d';
 CREATE ROLE middleman_visitor;
@@ -189,38 +188,38 @@ CREATE ROLE middleman_user;
 GRANT middleman_user TO sys_admin;
 
 CREATE TYPE m_pub.jwt_token AS (
-  role text,
+  role TEXT,
   person_id BIGINT
 );
 
 CREATE FUNCTION m_pub.authenticate(
-  email text,
-  password text
+  email TEXT,
+  password TEXT
 ) RETURNS m_pub.jwt_token AS $$
 declare
   account m_priv.person_account;
-begin
+BEGIN
   SELECT a.* INTO account
-  from m_priv.person_account AS a
-  where a.email = $1;
+  FROM m_priv.person_account AS a
+  WHERE a.email = $1;
 
-  if account.password_hash = crypt(password, account.password_hash) then
-    return ('middleman_user', account.person_id)::m_pub.jwt_token;
-  else
-    return null;
-  end if;
-end;
-$$ language plpgsql strict security definer;
+  IF account.password_hash = crypt(password, account.password_hash) THEN
+    RETURN ('middleman_user', account.person_id)::m_pub.jwt_token;
+  ELSE
+    RETURN NULL;
+  END IF;
+END;
+$$ LANGUAGE plpgsql strict security definer;
 
-COMMENT ON FUNCTION m_pub.authenticate(TEXT, TEXT) is 'Creates a JWT token that will securely identify a person and give them certain permissions.';
+COMMENT ON FUNCTION m_pub.authenticate(TEXT, TEXT) IS 'Creates a JWT token that will securely identify a person and give them certain permissions.';
 
 CREATE FUNCTION m_pub.current_person() RETURNS m_pub.person AS $$
   SELECT *
-  from m_pub.person
-  where id = current_setting('jwt.claims.person_id')::BIGINT
-$$ language sql stable;
+  FROM m_pub.person
+  WHERE id = current_setting('jwt.claims.person_id')::BIGINT
+$$ LANGUAGE sql stable;
 
-COMMENT ON FUNCTION m_pub.current_person() is 'Gets the person who was identified by our JWT.';
+COMMENT ON FUNCTION m_pub.current_person() IS 'Gets the person who was identified by our JWT.';
 
 CREATE FUNCTION m_pub.open_jobs(
   lat REAL,
@@ -233,23 +232,23 @@ CREATE FUNCTION m_pub.open_jobs(
   AND m_pub.job.category = job_type
   ORDER BY m_pub.job.geog <-> concat('SRID=26918;POINT(', long, ' ', lat, ')')::geometry
   LIMIT 50;
-$$ language sql stable;
+$$ LANGUAGE sql stable;
 
-COMMENT ON FUNCTION m_pub.open_jobs(REAL, REAL, m_pub.job_type) is 'Gets the 50 nearest open jobs given long lat and job type';
+COMMENT ON FUNCTION m_pub.open_jobs(REAL, REAL, m_pub.job_type) IS 'Gets the 50 nearest open jobs given long lat and job type';
 
 GRANT EXECUTE ON FUNCTION m_pub.open_jobs(REAL, REAL, m_pub.job_type) TO middleman_user;
 GRANT EXECUTE ON FUNCTION m_pub.authenticate(TEXT, TEXT) TO middleman_visitor, middleman_user;
 GRANT EXECUTE ON FUNCTION m_pub.current_person() TO middleman_visitor, middleman_user;
 GRANT EXECUTE ON FUNCTION m_pub.register_person(TEXT, TEXT, TEXT, TEXT) TO middleman_visitor;
-GRANT usage on schema m_pub TO middleman_visitor, middleman_user, sys_admin;
+GRANT usage on SCHEMA m_pub TO middleman_visitor, middleman_user, sys_admin;
 GRANT SELECT ON TABLE m_pub.person TO middleman_visitor, middleman_user;
 GRANT UPDATE, DELETE ON TABLE m_pub.person TO middleman_user;
-ALTER TABLE m_pub.person enable row level security;
-CREATE POLICY select_person on m_pub.person for SELECT TO middleman_user, middleman_visitor
+ALTER TABLE m_pub.person ENABLE ROW LEVEL SECURITY;
+CREATE POLICY select_person on m_pub.person FOR SELECT TO middleman_user, middleman_visitor
   USING (true);
-CREATE POLICY update_person on m_pub.person for UPDATE TO middleman_user
+CREATE POLICY update_person on m_pub.person FOR UPDATE TO middleman_user
   USING (id = current_setting('jwt.claims.person_id')::INTEGER);
-CREATE POLICY delete_person on m_pub.person for delete TO middleman_user
+CREATE POLICY delete_person on m_pub.person FOR delete TO middleman_user
   USING (id = current_setting('jwt.claims.person_id')::INTEGER);
 
 GRANT USAGE ON SEQUENCE m_pub.comment_id_seq TO middleman_user;
@@ -279,16 +278,16 @@ GRANT INSERT, UPDATE ON TABLE m_pub.comment_tree TO middleman_user;
 GRANT INSERT, UPDATE ON TABLE m_pub.person_comment TO middleman_user;
 GRANT INSERT, UPDATE ON TABLE m_pub.job TO middleman_user;
 
-ALTER TABLE m_pub.comment enable row level security;
+ALTER TABLE m_pub.comment ENABLE ROW LEVEL SECURITY;
 
-CREATE POLICY select_COMMENT ON m_pub.comment for SELECT TO middleman_user, middleman_visitor
+CREATE POLICY select_COMMENT ON m_pub.comment FOR SELECT TO middleman_user, middleman_visitor
   USING (true);
 
-CREATE POLICY insert_COMMENT ON m_pub.comment for INSERT TO middleman_user
+CREATE POLICY insert_COMMENT ON m_pub.comment FOR INSERT TO middleman_user
   WITH CHECK (person_id = current_setting('jwt.claims.person_id')::INTEGER);
 
-CREATE POLICY update_COMMENT ON m_pub.comment for UPDATE TO middleman_user
+CREATE POLICY update_COMMENT ON m_pub.comment FOR UPDATE TO middleman_user
   USING (person_id = current_setting('jwt.claims.person_id')::INTEGER);
 
-CREATE POLICY delete_COMMENT ON m_pub.comment for DELETE TO middleman_user
+CREATE POLICY delete_COMMENT ON m_pub.comment FOR DELETE TO middleman_user
   USING (person_id = current_setting('jwt.claims.person_id')::INTEGER);
