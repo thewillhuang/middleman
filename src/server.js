6c497b061700fbda2a0568351cc4510894befaa2
William Huang <will.h86@gmail.com>
2018-05-28 22:51:25 -0700

    readd morgan

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import { postgraphile } from 'postgraphile';
+import morgan from 'morgan';
 import cachedPool from './cachedPool';
 import {
   POSTGRAPHQLCONFIG,
   schemas,
+  isDevelopment,
   PGCONFIG,
 } from './config/index';
 
 const app = express();
+
+if (isDevelopment) {
+  app.use(morgan('dev'));
+}
 
 app.get('/', (req, res) => {
   res.send('pong');
